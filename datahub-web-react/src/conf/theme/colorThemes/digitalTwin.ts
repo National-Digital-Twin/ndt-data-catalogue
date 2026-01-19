f7e89eb32c6f12f0b58a93f333675dbf55434ec9
Kash Javed <kashif.javed@answerdigital.com>
2026-01-19 11:27:31 +0000

    feat(frontend): add new copyright headers

--- datahub-web-react/src/conf/theme/colorThemes/digitalTwin.ts
+++ datahub-web-react/src/conf/theme/colorThemes/digitalTwin.ts
@@ -1,12 +1,10 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * This file is unmodified from its original version developed by Acryl Data, Inc.,
- * and is now included as part of a repository maintained by the National Digital Twin Programme.
- * All support, maintenance and further development of this code is now the responsibility
- * of the National Digital Twin Programme.
+ * © Crown Copyright 2025. This work has been developed by the National Digital Twin
+ * Programme and is legally attributed to the Department for Business and Trade (UK) as the governing
+ * entity
  */
-import { ColorValues } from '@components/theme/config';
 import colors from '@conf/theme/colorThemes/color';
 import ColorTheme from '@conf/theme/colorThemes/types';
 
